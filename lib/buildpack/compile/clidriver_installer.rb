d5a2574ccd8daa738f73a8e7d0b18ed60b734f4a
PRASHANTHRAOK <PrashanthIBM@users.noreply.github.com>
2016-04-28 18:19:49 +0530

    Update clidriver_installer.rb

--- lib/buildpack/compile/clidriver_installer.rb
+++ lib/buildpack/compile/clidriver_installer.rb
@@ -24,14 +24,15 @@ module AspNet5Buildpack
     def install(app_dir, out)
       @shell.env['HOME'] = app_dir
 	  
-      cmd = 'touch ~/.bashrc; curl -LO ftp://9.26.93.131/devinst/db2_v105fp6/linuxamd64/s150623/v10.5fp6_linuxx64_odbc_cli.tar.gz; rm -rf  #{app_dir}/odbc_cli; '
+     # cmd = 'touch ~/.bashrc; curl -LO ftp://9.26.93.131/devinst/db2_v105fp6/linuxamd64/s150623/v10.5fp6_linuxx64_odbc_cli.tar.gz; rm -rf  #{app_dir}/odbc_cli; '
+     cmd = 'touch ~/.bashrc; rm -rf  #{app_dir}/odbc_cli; '
       @shell.exec(cmd, out)
 	  
-      cmd = 'tar -xvzf v10.5fp6_linuxx64_odbc_cli.tar.gz -C #{app_dir} &> /dev/null'
+      cmd = 'tar -xvzf #{app_dir}/v10.5fp6_linuxx64_odbc_cli.tar.gz -C #{app_dir} &> /dev/null'
       @shell.exec(cmd, out)
 	  
-      cmd = 'cp -rf #{app_dir}/libdb2.so.1 #{app_dir}/odbc_cli/clidriver/lib/libdb2.so.1'
-      @shell.exec(cmd, out)
+      #cmd = 'cp -rf #{app_dir}/libdb2.so.1 #{app_dir}/odbc_cli/clidriver/lib/libdb2.so.1'
+     # @shell.exec(cmd, out)
 	  
       @shell.env['LD_LIBRARY_PATH'] = "$LD_LIBRARY_PATH:#{app_dir}/odbc_cli/clidriver/lib"
       @shell.env['PATH'] = "$PATH:#{app_dir}/odbc_cli/clidriver/bin"
