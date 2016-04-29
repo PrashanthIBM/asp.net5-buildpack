be48176bc43147ed6d6b11b33aba00d0d78cb9b2
PRASHANTHRAOK <PrashanthIBM@users.noreply.github.com>
2016-04-29 17:55:07 +0530

    Update clidriver_installer.rb

--- lib/buildpack/compile/clidriver_installer.rb
+++ lib/buildpack/compile/clidriver_installer.rb
@@ -45,7 +45,7 @@ module AspNet5Buildpack
       cmd = 'echo $LD_LIBRARY_PATH; echo $PATH; ls -lRt #{app_dir}/clidriver/odbc_cli/clidriver/bin; '
       @shell.exec(cmd, out)
       
-      cmd = /bin/cp -Rvf #{app_dir}/db2dsdriver.cfg #{app_dir}/clidriver/odbc_cli/clidriver/cfg '
+      cmd = '/bin/cp -Rvf #{app_dir}/db2dsdriver.cfg #{app_dir}/clidriver/odbc_cli/clidriver/cfg '
       #cmd = echo 
       #; db2cli validate -dsn alias1 -connect '
       #cmd = 'db2cli validate'
